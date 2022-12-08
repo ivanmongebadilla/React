a0609c5f6da95f8d24bea09c181ac29cb13fc406
Ivan Monge <ivanmongebadilla@gmail.com>
2022-12-07 18:06:44 -0800

    state and hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
 
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
---
+++ src/features/campsites/CampsiteDetail.js
@@ -0,0 +1,16 @@
+import { Card, CardImg, CardText, CardBody, Col } from 'reactstrap';
+
+
+const CampsiteDetail = ({ campsite }) => {
+    const { image, name, description } = campsite;
+  return (
+    <Col md='12' className='m-4'>
+        <Card>
+            <CardImg top src={image} alt={name} />
+            <CardText>{description}</CardText>
+        </Card>
+    </Col>
+  );
+}
+
+export default CampsiteDetail
