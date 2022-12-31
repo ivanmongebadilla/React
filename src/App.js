b34bcc646e8c380ca2542a2b1430cf04c59c86e4
Ivan Monge <ivanmongebadilla@gmail.com>
2022-12-31 12:25:41 -0800

    Workshop 5

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
 import { fetchPromotions } from './features/promotions/promotionsSlice';
+import { fetchComments } from './features/comments/commentsSlice';
 import './App.css';
 import AboutPage from './pages/AboutPage';
 
@@ -21,6 +22,7 @@ function App() {
     dispatch(fetchCampsites());
     dispatch(fetchPartners());
     dispatch(fetchPromotions());
+    dispatch(fetchComments());
   }, [dispatch]);
 
   return (
